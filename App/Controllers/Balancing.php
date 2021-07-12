0646a63e7fe8ef963b8bc19c4ac12001a55a872b
Bartlomiej <bartlomiej.w.programista@gmail.com>
2021-07-12 19:33:34 +0200

    Add/Balancing controller

---
+++ App/Controllers/Balancing.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+use \App\Models\Income;
+use \App\Models\IncomeCategories;
+
+class Incoming extends \Core\Controller
+{
+    protected function before()
+    {
+        session_start();
+    }
+	
+	public function getItemsAction()
+    {
+      
+    }
+
+	public function newAction()
+    {
+		$categories=static::loadCategories();
+		View::renderTemplate('Incoming/new.html', ['categories' => $categories]);
+    }
+	
+	public function addAction()
+    {
+        $income= new Income($_POST);
+		$userId=$_SESSION['loggedUserId'];
+		$incomeCategories = new IncomeCategories($userId);
+		$categoryId = $incomeCategories -> getCategoryId($income->item);
+		$income->setUserId($userId);
+		$income->setCategoryId($categoryId);
+		
+		if( $income->save() )
+		{
+			View::renderTemplate('Incoming/success.html');
+		}
+		else
+		{
+			$categories=static::loadCategories();
+			View::renderTemplate('Incoming/new.html', ['categories' => $categories, 'income' => $income]);
+		}
+    }
+	
+	public static function loadCategories()
+	{
+		$userId=$_SESSION['loggedUserId'];
+		$incomeCategories = new IncomeCategories($userId);
+		$categories=$incomeCategories-> getCategoriesById($userId) ;
+		return $categories;
+	}
+	
+	public function successAction()
+    {
+        View::renderTemplate('MainSite/mainSite.html');
+    }
+
+}
