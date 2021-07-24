fc280cad8193b13b2b756e9a17693f3c836bb1c4
Bartlomiej <bartlomiej.w.programista@gmail.com>
2021-07-24 19:34:41 +0200

    Showing income diagram

---
+++ public/js/DiagramExpenses.js
@@ -0,0 +1,26 @@
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+		var count=namesArray.length;
+		var dataMatrix= [];
+		dataMatrix[0] = ['Task', 'Hours per Day'];
+		 for( var i=0 ; i < count ; i++)
+		 {
+			 dataMatrix[i+1]=[ namesArray[ i ], amountsArray[ i ] ];
+		 } 
+		
+        var data = google.visualization.arrayToDataTable(dataMatrix);
+
+        var options = {
+          title: '',
+		  backgroundColor: '#30B2C7',
+		  chartArea:{left:20,top:20,width:'60%',height:'75%'},
+		  legend: {textStyle: {color: 'white'}}
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechartIncomes'));
+
+        chart.draw(data, options);
+      }
